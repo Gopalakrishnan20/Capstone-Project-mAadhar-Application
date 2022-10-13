c132e63b8fc8f8d0e0a4b19b715b871f439a854e
Gopalakrishnan20 <gopalakrishnan567890@gmail.com>
2022-10-13 22:56:46 +0530

    Admin Dashboard

--- Frontend/aadhar_app/src/app/adminlogin/adminlogin.component.ts
+++ Frontend/aadhar_app/src/app/adminlogin/adminlogin.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Admin } from '../services/admin';
 import {AdminServiceService} from '../services/admin-service.service';
 import { Router } from '@angular/router';
@@ -15,13 +15,22 @@ export class AdminloginComponent implements OnInit {
   signupForm: FormGroup;
   admin:Admin=new Admin();
 
-  constructor(private adminServ:AdminServiceService, private router: Router) { }
+  constructor(private adminServ:AdminServiceService, private router: Router,private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.signupForm = this.fb.group({
+      uName: 'admin',
+      pass: 'Qwerty@098!'
+    });
   }
 
   onSubmit() {
-    this.router.navigate(['/admindashboard']);
+    if(this.signupForm.value.uName=="admin" && this.signupForm.value.pass=="Qwerty@098!"){
+      this.router.navigate(['/admindashboard']);
+    }
+    else{
+      alert("Invalid credentials");
+    }
   }
 
 }
